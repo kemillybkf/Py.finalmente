633979850ef45b89d5b7fac6d1ba461b11f21872
kemillybkf <kemillybkf@gmail.com>
2025-06-06 17:35:45 -0300

    Add files via upload

---
+++ ket25.py
@@ -0,0 +1,16 @@
+
+
+cad = int(input('Quantas pessoas você deseja cadastrar?'))
+print('-'*40)
+while  :
+    n1 = int(input('Digite o valor do seu salário: '))
+    n2 = int(input('Digite a quantidade de filhos que você tem: '))
+    print('-'*40)
+conta = cad + 1
+
+
+
+salario = n1/cad
+
+if n1 :
+    print('A média so salário da população é: {} '.format(salario)) 
