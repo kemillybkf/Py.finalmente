629f3eff5a3d153abeaa03ba4cbe123b5a9cb9da
kemillybkf <kemillybkf@gmail.com>
2025-06-10 16:41:21 -0300

    Add files via upload

---
+++ ket40.py
@@ -0,0 +1,6 @@
+def saudacao(msg='Olá, tudo bem!', nome='Usuário'):
+    print(msg, nome)
+def linha():
+    print('='*30)
+
+saudacao()
---
+++ ket41.py
@@ -0,0 +1,8 @@
+def divisao(n1,n2):
+    return(n1/n2)
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+res = divisao(n1, n2)
+print(res)
